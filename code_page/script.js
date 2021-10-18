39fcda87f586eb47c3f12ef7be67e51427e69af4
Jingiru <skyjjw79@hanmail.net>
2021-10-19 03:20:28 +0900

    page2 개선 테스트

--- code_page/script.js
+++ code_page/script.js
@@ -39,3 +39,91 @@ function to1() {
 	});
 
 }
+
+
+function to2() {
+	let input = document.getElementById("input");
+	let error = document.getElementById("errorname");
+	let check = document.getElementById("check");
+	
+	//시작 전 경고 문구와 애니메이션 초기화
+	error.innerHTML = "";
+	input.style.animation='';
+
+	function ans()
+	{
+		if(input.value == "라온이")
+			location.href="./code_page/page2_raon.html";
+		else
+		{
+			input.style.animation='shake 0.5s';
+			error.innerHTML = "다시 풀어보세요";
+		}
+	}
+
+	//타이핑을 했을 때
+	input.addEventListener("keydown", e => {
+		//엔터라면 답안 확인(ans 함수 호출)
+		if(e.keyCode === 13) {
+			ans();
+		}
+	});
+
+	//입력창을 클릭했을 때
+	input.addEventListener("click", () => {
+		error.innerHTML = "";
+		input.style.animation='';
+	});
+
+
+	//확인 버튼을 클릭했을 때
+	check.addEventListener("click", () => {
+		ans();
+	});
+
+}
+
+
+function to3() {
+	let input = document.getElementById("input");
+	let error = document.getElementById("errorname");
+	let check = document.getElementById("check");
+	
+	//시작 전 경고 문구와 애니메이션 초기화
+	error.innerHTML = "";
+	input.style.animation='';
+
+	function ans()
+	{
+		if(input.value == "지도학습")
+			location.href="./code_page/page3_ml.html";
+		else
+		{
+			input.style.animation='shake 0.5s';
+			error.innerHTML = "다시 풀어보세요";
+		}
+	}
+
+	//타이핑을 했을 때
+	input.addEventListener("keydown", e => {
+		//엔터라면 답안 확인(ans 함수 호출)
+		if(e.keyCode === 13) {
+			ans();
+		}
+	});
+
+	//입력창을 클릭했을 때
+	input.addEventListener("click", () => {
+		error.innerHTML = "";
+		input.style.animation='';
+	});
+
+
+	//확인 버튼을 클릭했을 때
+	check.addEventListener("click", () => {
+		ans();
+	});
+
+}
+
+
