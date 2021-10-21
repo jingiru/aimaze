f37f7ce84a05ece8b1ab3002f7fa6febf9c30e0f
Jingiru <skyjjw79@hanmail.net>
2021-10-22 05:22:05 +0900

    4단계 넘어가는 함수 추가 4번째 방 새로 생성

--- code_page/script.js
+++ code_page/script.js
@@ -127,3 +127,45 @@ function to3() {
 }
 
 
+
+function to4() {
+	let input = document.getElementById("input");
+	let error = document.getElementById("errorname");
+	let check = document.getElementById("check");
+	
+	//시작 전 경고 문구와 애니메이션 초기화
+	error.innerHTML = "";
+	input.style.animation='';
+
+	function ans()
+	{
+		if(input.value == "40")
+			location.href="./page4_40.html";
+		else
+		{
+			input.style.animation='shake 0.5s';
+			error.innerHTML = "다시 풀어보세요";
+		}
+	}
+
+	//타이핑을 했을 때
+	input.addEventListener("keydown", e => {
+		//엔터라면 답안 확인(ans 함수 호출)
+		if(e.keyCode === 13) {
+			ans();
+		}
+	});
+
+	//입력창을 클릭했을 때
+	input.addEventListener("click", () => {
+		error.innerHTML = "";
+		input.style.animation='';
+	});
+
+
+	//확인 버튼을 클릭했을 때
+	check.addEventListener("click", () => {
+		ans();
+	});
+
+}
