8affb0a18759e9be22d7961f7608815b037b10c9
jingiru <skyjjw79@gmail.com>
2021-10-22 11:50:50 +0900

    그림에서 W8을 W9로 수정 다음 페이지 넘어가는 함수 추가 page5 생성

--- code_page/script.js
+++ code_page/script.js
@@ -127,7 +127,6 @@ function to3() {
 }
 
 
-
 function to4() {
 	let input = document.getElementById("input");
 	let error = document.getElementById("errorname");
@@ -141,6 +140,49 @@ function to4() {
 	{
 		if(input.value == "40")
 			location.href="./page4_40.html";
+		else
+		{
+			input.style.animation='shake 0.5s';
+			error.innerHTML = "다시 풀어보세요";
+		}
+	}
+
+	//타이핑을 했을 때
+	input.addEventListener("keydown", e => {
+		//엔터라면 답안 확인(ans 함수 호출)
+		if(e.keyCode === 13) {
+			ans();
+		}
+	});
+
+	//입력창을 클릭했을 때
+	input.addEventListener("click", () => {
+		error.innerHTML = "";
+		input.style.animation='';
+	});
+
+
+	//확인 버튼을 클릭했을 때
+	check.addEventListener("click", () => {
+		ans();
+	});
+
+}
+
+
+function to5() {
+	let input = document.getElementById("input");
+	let error = document.getElementById("errorname");
+	let check = document.getElementById("check");
+	
+	//시작 전 경고 문구와 애니메이션 초기화
+	error.innerHTML = "";
+	input.style.animation='';
+
+	function ans()
+	{
+		if(input.value == "g3" || input.value == "G3")
+			location.href="./page5_g3.html";
 		else
 		{
 			input.style.animation='shake 0.5s';
