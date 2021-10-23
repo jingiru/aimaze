e12d6d5b3ed0c6c32faba2932c129032380af862
Jingiru <skyjjw79@hanmail.net>
2021-10-23 21:17:29 +0900

    page9까지 완성 value 형 변환 필요없음

--- code_page/script.js
+++ code_page/script.js
@@ -395,7 +395,50 @@ function to10() {
 
 	function ans()
 	{
-		if(input.value == "g3" || input.value == "G3")
+		if(Number(input.value) >= 2239 && Number(input.value) <= 2259)
+			location.href="./page10_22392259.html";
+		else
+		{
+			input.style.animation='shake 0.5s';
+			error.innerHTML = "다시 풀어보세요";
+		}
+	}
+
+	//타이핑을 했을 때
+	input.addEventListener("keydown", e => {
+		//엔터라면 답안 확인(ans 함수 호출)
+		if(e.keyCode === 13) {
+			ans();
+		}
+	});
+
+	//입력창을 클릭했을 때
+	input.addEventListener("click", () => {
+		error.innerHTML = "";
+		input.style.animation='';
+	});
+
+
+	//확인 버튼을 클릭했을 때
+	check.addEventListener("click", () => {
+		ans();
+	});
+
+}
+
+
+function toend() {
+	let input = document.getElementById("input");
+	let error = document.getElementById("errorname");
+	let check = document.getElementById("check");
+	
+	//시작 전 경고 문구와 애니메이션 초기화
+	error.innerHTML = "";
+	input.style.animation='';
+
+	function ans()
+	{
+		if(input.value >= 2239 && input.value <= 2259)
 			location.href="./page5_g3.html";
 		else
 		{
